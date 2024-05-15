da02455ccaa8796216e83aab288d8447f400d4d7
lbbrowns <38124010+lb-browns@users.noreply.github.com>
2024-05-15 16:21:18 -0700

    Finished
    Finished all the randomizer functions and built GUI pretty much done with this project

--- EftKitRandV3.py
+++ EftKitRandV3.py
@@ -3,28 +3,17 @@ from io import StringIO
 import requests
 import pandas as pd
 import numpy as np
+import tkinter as tk
 
-def getRandomHelmet():
- helmetWikiLink = 'https://escapefromtarkov.fandom.com/wiki/Headwear'
+window = tk.Tk()
+window.title("Escape From Tarkov Kit Randomizer")
+window.geometry("800x500")
 
- helmetSoupA = BeautifulSoup(requests.get(helmetWikiLink).content, 'html.parser')
-
- helmetDF = []
-
- for i in helmetSoupA.select(".wikitable"):
-    for hr in i.select("hr"):
-     hr.replace_with(", ")
-    helmetDf = pd.read_html(StringIO(str(i)))[0]
-    helmetTitle = i.find_previous("h2").span.text
-    helmetDf["helmet_type"] = helmetTitle
-    helmetDF.append(helmetDf)
-
- helmetDf_out = pd.concat(helmetDF)
- print(helmetDf_out)
- randHelm = helmetDf_out.sample(n=1)
-
- print(randHelm[["Name", "Icon"]])
-
+weaponLabel = tk.Label(window, text="weapon")
+backpackLabel = tk.Label(window, text="backpack")
+armorLabel = tk.Label(window, text="armor")
+rigLabel = tk.Label(window, text="rig")
+helmetLabel = tk.Label(window, text="helmet")
 
 
 def getRandomGun():
@@ -53,9 +42,9 @@ def getRandomGun():
 
  randGun = big_df.sample(n=1)
 
- print (randGun[["Name", "Image"]])
+ return randGun[["Name"]]
 
- #Simple Way To Do This
+ #Other Way To Do This Just Listen To Compiler
  #reqWeapons = requests.get(weaponWikiLink, "lxml")
  #weaponsSoup = BeautifulSoup(reqWeapons.content)
  #weaponsTableA = weaponsSoup.find_all(name='table')
@@ -67,6 +56,111 @@ def getRandomGun():
 
  #print (dfAR)
  #print (dfSMG)
+
+def getRandomHelmet():
+ helmetWikiLink = 'https://escapefromtarkov.fandom.com/wiki/Headwear'
+
+ headwearSoupA = BeautifulSoup(requests.get(helmetWikiLink).content, 'html.parser')
+
+ headwearDF = []
+
+ for i in headwearSoupA.select(".wikitable"):
+    for hr in i.select("hr"):
+        hr.replace_with(", ")
+    df = pd.read_html(StringIO(str(i)))[0]
+    title = i.find_previous("h2").span.text
+    df["headwear_type"] = title
+    headwearDF.append(df)
     
 
-getRandomGun()
+ df_out = pd.concat(headwearDF)
+
+ big_df = pd.concat([df_out[df_out['headwear_type'].isin(["Armored", "Vanity"])]])
+ big_df.reset_index(drop=True, inplace=True)
+
+ randHeadwear = big_df.sample(n=1)
+ 
+ return randHeadwear[['Name']]
+
+def getRandChestRig():
+   chestRigWikiLink = 'https://escapefromtarkov.fandom.com/wiki/Chest_rigs'
+   chestRigSoupA = BeautifulSoup(requests.get(chestRigWikiLink).content, 'html.parser')
+
+   chestRigDF = []
+
+   for i in chestRigSoupA.select(".wikitable"):
+    for hr in i.select("hr"):
+        hr.replace_with(", ")
+    df = pd.read_html(StringIO(str(i)))[0]
+    title = i.find_previous("h2").span.text
+    df["chest_rigs_type"] = title
+    chestRigDF.append(df)
+    
+
+   df_out = pd.concat(chestRigDF)
+
+   big_df = pd.concat([df_out[df_out['chest_rigs_type'].isin(["Unarmored"])]])
+   big_df.reset_index(drop=True, inplace=True)
+
+   randChestRig = big_df.sample(n=1)
+   
+   return randChestRig[['Name']]
+
+def getRandomAV():
+ avWikiLink = 'https://escapefromtarkov.fandom.com/wiki/Armor_vests'
+
+ 
+ avSoup = BeautifulSoup(requests.get(avWikiLink).content, 'html.parser')
+ avTableA = avSoup.find_all(name='table')
+
+ dfAV = pd.read_html(StringIO(str(avTableA)))[0]
+ dfAV.head()
+ RandAv = dfAV.sample(n=1)
+
+ return RandAv[['Name']]
+ 
+def getRandomBackpack():
+ 
+ backpackWikiLink = 'https://escapefromtarkov.fandom.com/wiki/Backpacks'
+
+ 
+ backpackSoup = BeautifulSoup(requests.get(backpackWikiLink).content, 'html.parser')
+ backpackTableA = backpackSoup.find_all(name='table')
+
+ dfBackpack = pd.read_html(StringIO(str(backpackTableA)))[0]
+ dfBackpack.head()
+ RandBackpack = dfBackpack.sample(n=1)
+
+ return RandBackpack[['Name']]
+
+def displayWindowText():
+  
+  weapon = getRandomGun()
+  backpack = getRandomBackpack()
+  armor = getRandomAV()
+  rig = getRandChestRig()
+  helmet = getRandomHelmet()
+
+  weaponLabel.config(text=weapon)
+  backpackLabel.config(text=backpack)
+  armorLabel.config(text=armor)
+  rigLabel.config(text=rig)
+  helmetLabel.config(text=helmet)
+
+
+
+def displayWindow():
+
+  weaponLabel.pack()
+  backpackLabel.pack()
+  armorLabel.pack()
+  rigLabel.pack()
+  helmetLabel.pack()
+
+  Tbutton = tk.Button(window, text="Randomize",command=displayWindowText)
+
+  Tbutton.pack()
+
+  window.mainloop()
+
+displayWindow()
